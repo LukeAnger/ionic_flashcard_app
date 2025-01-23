8259f0864c7d5b74b1007d47730d4232011ba422
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2025-01-23 16:57:22 -0500

    Updated Typings

--- src/components/quiz/quizTypings.ts
+++ src/components/quiz/quizTypings.ts
@@ -1,29 +1,41 @@
 export interface Question {
-    Question: string;
-    A: string;
-    B: string;
-    C: string;
-    D: string;
-    CorrectAnswer: string;
-    MomentID: number;
-    LectureID: number;
-    Topics: string[];
-    QuestionID: string;
-    Reported: boolean;
-    Hint: string;
-    ExplanationForOptions: {
-        A: string;
-        B: string;
-        C: string;
-        D: string;
-    };
-    MatchCount: number;
-  }
+  Question: string;
+  A: string;
+  B: string;
+  C: string;
+  D: string;
+  ["Correct Answer"]: string;
+  ["Moment ID"]: number;
+  ["Lecture ID"]: number;
+  Topics: string[];
+  ["Question ID"]: string;
+  Reported: boolean;
+  Hint: string;
+  ["Explanation For Options"]: {
+      A: string;
+      B: string;
+      C: string;
+      D: string;
+  };
+  ["Match Count"]: number;
+}
+
+export interface Answer {
+  answer: string;
+  selected: boolean;
+  correct: boolean;
+  handleSelectAnswer: (answer: string) => void;
+}
   
 export interface QuizListProps {
-questions: Question[];
+  questions: Question[];
 }
 
 export interface QuizListItemProps {
   question: Question;
+  handleSelectAnswer: (answer: string) => void;
+}
+
+export interface AnswerProps {
+  answer: Answer;
 }
