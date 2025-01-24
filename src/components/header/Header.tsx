fcd35769c93b237b4aec41c5778f3ff4f96f0ede
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2025-01-24 14:14:46 -0500

    Updated header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,19 +10,24 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({showBack}) => {
-    // console.log("Header: ", showBack);
+    console.log("Header: ", showBack);
     
     return (
         <IonHeader className="row jc-sb ai-c" id="app-header">
-            {showBack ? (
-                <IonButton expand="block" fill="clear" routerLink="/home">
-                    <IonIcon icon={returnUpBack} />
-                </IonButton>
-            ) : (
-                <div style={{color: "black"}}>PH</div>
-            )}
-            <CluesLogo />
-            <div>
+            <div id="app-header-1" className="grid-center">
+                {showBack ? (
+                    <IonButton expand="block" fill="clear" routerLink="/home">
+                        <IonIcon icon={returnUpBack} />
+                    </IonButton>
+                ) : (
+                    <div style={{color: "black"}}>PH</div>
+                )}
+            </div>
+            <div id="app-header-2">
+                
+                <CluesLogo />
+            </div>
+            <div id="app-header-3" className="grid-center">
                 PH
             </div>
         </IonHeader>
